f107feed8bf4cf492c0699e379a352ba5272d9e4
salasjc <salasjc@HRD6402574>
2024-02-17 04:14:50 -0600

    Add project files.

---
+++ ExamenEjercicio2/Estudiante.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenEjercicio2
+{
+    class Estudiante
+    {
+        private double examen1;
+        private double examen2;
+        private double final;
+
+        // constructores para asignarles valores a las notas
+        public double Examen1
+        {
+            get { return examen1; }
+            set { examen1 = value; }
+        }
+
+        public double Examen2
+        {
+            get { return examen2; }
+            set { examen2 = value; }
+        }
+
+        public double Final
+        {
+            get { return final; }
+            set { final = value; }
+        }
+        // metodo para calular la nota final del estudiante:
+        public void CalcularFinal()
+        {
+            Final = (Examen1 + Examen2) / 2;
+        }
+
+        /* este metodo lo utilice para compara la nota final, asigando un numero minimo para ver si pasaba o no el curso.
+         * y mostrando el mensaje que corresponde
+         */
+        
+        public void MostrarResultado()
+        {
+            Console.WriteLine($"Nota final: {Final}");
+            if (Final >= 70)
+            {
+                Console.WriteLine("¡El estudiante aprobo el curso!");
+            }
+            else
+            {
+                Console.WriteLine("El estudiante reprobo el curso.");
+            }
+        }
+    }
+}
---
+++ ExamenEjercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExamenEjercicio2
+{
+    internal class Program
+    {
+        /*
+         * Este metodo lo que hace es solicitar al usuario las notas,
+         * utiliza el metodo CalcularFinal para hacer el promedio de notas,
+         * y en el metodo MostrarResultado nos brinda el mensaje si paso o no el curso. 
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la nota del primer examen:");
+            double notaExamen1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la nota del segundo examen:");
+            double notaExamen2 = Convert.ToDouble(Console.ReadLine());
+
+            Estudiante estudiante = new Estudiante();
+            estudiante.Examen1 = notaExamen1;
+            estudiante.Examen2 = notaExamen2;
+            estudiante.CalcularFinal();
+            estudiante.MostrarResultado();
+        }
+    }
+}
